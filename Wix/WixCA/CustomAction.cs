2f24b05be49f3e58dddefa232c69711a2e675d53
Sean Tearney <sean@ispyconnect.com>
2015-10-29 16:26:36 +0800

    Added wix updates to restore compatibility with rebrander application

---
+++ Wix/WixCA/CustomAction.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.Deployment.WindowsInstaller;
+
+namespace WixCA
+{
+    public class CustomActions
+    {
+        [CustomAction]
+        public static ActionResult CopyCustomisationFiles(Session session)
+        {
+            session.Log("Begin CopyCustomisationFiles");
+
+            string path = session.CustomActionData["SourceDir"];
+
+            session.Log("source dir is " + path);
+
+            string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\iSpy\";
+            path = path.Trim().Trim('\\') + @"\";
+            try
+            {
+                if (File.Exists(path + "custom.txt"))
+                {
+                    TryCopy(path + @"custom.txt", appDataPath + @"custom.txt", true);
+                    TryCopy(path + @"logo.jpg", appDataPath + @"logo.jpg", true);
+                    TryCopy(path + @"logo.png", appDataPath + @"logo.png", true);
+                }
+
+            }
+            catch
+            {
+
+            }
+
+
+            return ActionResult.Success;
+        }
+
+        private static void TryCopy(string source, string target, bool overwrite)
+        {
+            try
+            {
+                File.Copy(source, target, overwrite);
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
