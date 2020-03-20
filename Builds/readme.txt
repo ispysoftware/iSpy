9759343e552ca55c00162c0b56f8180acb91006f
sean tearney <sean@ispyconnect.com>
2020-03-20 11:32:26 +0800

    fixes for code signing (internal)

--- Builds/readme.txt
+++ Builds/readme.txt
@@ -1,1 +1,2 @@
 Release build installers end up here
+We code sign release builds and the signing certificate and related batch files are not part of the open source code.
--- Wix/Burn_Debug.bat
+++
@@ -1,2 +0,0 @@
-"C:\Program Files (x86)\WiX Toolset v3.11\bin\insignia.exe" -ib "%~dp0Bootstrap\bin\Debug\iSpySetup.exe" -o "%~dp0Bootstrap\bin\Debug\engine.exe"
-"C:\Program Files (x86)\WiX Toolset v3.11\bin\insignia.exe" -ab "%~dp0Bootstrap\bin\Debug\engine.exe" "%~dp0Bootstrap\bin\Debug\iSpySetup.exe" -o "%~dp0Bootstrap\bin\Debug\iSpySetup.exe"
